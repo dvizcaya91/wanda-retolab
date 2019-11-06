5377fe77c260d288fbc804e6a62877ebce8ad9ee
Hyatt <hyattsaleh@gmail.com>
2019-11-06 15:22:23 -0500

    Front

--- wandaapp/views.py
+++ wandaapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def index(request):
     
     context = {}
-    return render(request, 'index.html', context)
+    return render(request, 'wandaapp/dashboard-finance.html', context)
 
 
 # Create your views here.
